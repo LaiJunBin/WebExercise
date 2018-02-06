ea4f03e293999e24835adc7f7ce991129ed52f3f
xyz607xx <xyz607xx@gmail.com>
2018-02-06 14:04:24 +0800

    負責路由切換畫面

--- skill01-login/assets/js/main.js
+++ skill01-login/assets/js/main.js
@@ -1,9 +1,25 @@
 addCSS('head', './assets/css/main.css');
-req = new XMLHttpRequest();
-req.open('get', './assets/html/loginForm.html');
-req.onload = function () {
-    var main = document.getElementById('main');
-    addScript('#main', './assets/js/loginForm.js');
-    main.innerHTML = this.responseText;
-};
-req.send();
+window.onhashchange = function () {
+    route = location.hash.replace('#/', '');
+    switch (route) {
+        case '':
+        case 'login':
+            document.title = '登入';
+            updateContent('#main', 'get', './assets/html/loginForm.html', ['./assets/js/loginForm.js']);
+            break;
+        case 'join':
+            document.title = '註冊';
+            updateContent('#main', 'get', './assets/html/joinMemberForm.html',
+                ['./assets/js/loginForm.js', './assets/js/join.js']);
+            break;
+        case 'search':
+            document.title = '查詢';
+            updateContent('#main', 'get', './assets/html/search.html', ['./assets/js/search.js']);
+            break;
+        default:
+            updateContent('#main', 'get', './assets/html/error404.html');
+            break;
+
+    }
+}
+window.onhashchange();
