e90d2937839f58f818f979d9523390ffbaf02e97
xyz607xx <xyz607xx@gmail.com>
2018-04-25 11:27:00 +0800

    表格加入框線，按鈕縮小

--- 48區賽模擬試題/第五次做/assets/main.js
+++ 48區賽模擬試題/第五次做/assets/main.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 $(function(){
 	document.title = $(".container>header").text();
-	$(".fill,button").button();
+	$(".fill,button").button().removeClass('ui-widget');
 	$("input[type!=radio],textarea").addClass('input');
-	$(".btnSet button").addClass('ui-controlgroup-item').removeClass('ui-widget');
+	$(".btnSet button").addClass('ui-controlgroup-item');
 });
